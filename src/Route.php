b7d54765a4eba4659bcdc4665eb7aba0e1062fb7
kevinho19 <kevin.b_19@hotmail.fr>
2020-04-16 11:55:12 +0200

    maj

---
+++ src/Route.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Route{
+
+    protected $_path;
+
+    protected $_callable;
+
+    public function __construct($path, $callable){
+        $this->_path = $path;
+        $this->_callable = $callable;
+    }
+}
